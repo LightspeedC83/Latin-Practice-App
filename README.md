cc74e144d0601884507684a22f57a1c44e8e1011
Chase Lowney <chasea2016@gmail.com>
2023-02-17 23:37:36 -0500

    added list of first declension nouns -have yet to integrate them into class system

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Latin Practice App
 
 a program/application to help study the forms and meanings of Latin words
+
+# # sources:
+- 6 years of Latin
+- https://quizlet.com/240560327/latin-nouns-first-declension-flash-cards/
+- https://quizlet.com/824589/latin-first-declension-nouns-flash-cards/
+-
---
+++ word_files/noun_lists/combining_noun_lists.py
@@ -0,0 +1,42 @@
+with open("word_files/noun_lists/noun_list_first_combined.txt", "w", encoding = "utf-8") as output, open("word_files/noun_lists/noun_list_first_1.txt") as f1, open("word_files/noun_lists/noun_list_first_2.txt", encoding="utf8") as f2: 
+    # getting and formatting list of words from file 1
+    list_a = []
+    for entry in f1:
+        list_a.append([entry[0:entry.index(")")+1].strip(), entry[entry.index(")")+1:].strip()])
+
+    # getting and formatting list of words from file 2
+    list_b = []
+    i = 1
+    temp = None
+    check = False
+    for entry in f2:
+        if i % 2 == 1 and not entry.strip()=="":
+            temp = entry
+            check = True
+        elif check:
+            list_b.append([temp.strip(), entry.strip()])
+            check = False
+        i+=1
+
+    # comparing the two lists for repeated words
+    matching = []
+    for x in list_a:
+        for y in list_b:
+            if x[0][0:x[0].index(",")] == y[0][0:y[0].index(",")]:
+                matching.append(x)
+                break
+    
+    # writing the combined list to the output file
+    for x in list_a:
+        if x not in matching:
+            output.write(x[0] + " | " + x[1] + "\n")
+    for y in list_b:
+        match = False
+        for m in matching:
+           if m[0][0:m[0].index(",")] == y[0][0:y[0].index(",")]:
+               match = True
+        
+        if not match:
+            output.write(y[0] + " | " + y[1] + "\n")
+    for m in matching:
+        output.write(m[0] + " | " + m[1] + "\n")
